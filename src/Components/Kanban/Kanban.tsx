dccc00a31cb15ed6e57fcdae9f1b26256b888391
Jilshak <mohd.jilshak@gmail.com>
2023-01-29 11:11:12 +0530

    added some button functionality to kanban board

--- src/Components/Kanban/Kanban.tsx
+++ src/Components/Kanban/Kanban.tsx
@@ -4,22 +4,58 @@ import { useDrag } from 'react-dnd'
 
 function Kanban() {
   const [input, setInput] = useState("")
-  const [tasks, setTasks] = useState<string[]>([])
+  const [todos, setTodos] = useState<string[]>([])
+  const [inProgress, setProgress] = useState<string[]>([])
+  const [done, setDone] = useState<string[]>([])
 
   //this function will only add task to the todo if there-
   // actually is some content in the input field
-  const handleAddTask = (e: any) => {
+  const handleAddtodo = (e: any) => {
     if (e.key === "Enter" && input !== '') {
-      setTasks([...tasks, input])
+      setTodos([...todos, input])
       setInput("")
     }
   }
 
-  //function to delete task from the todo
-  const handleDeleteTask = (index: number) => {
-    let newTasks = [...tasks]
-    newTasks.splice(index, 1)
-    setTasks(newTasks)
+  //functions to delete tasks from the todo/inprogress and done
+  const handleDeletetodo = (index: number) => {
+    let newtodos = [...todos]
+    newtodos.splice(index, 1)
+    setTodos(newtodos)
+  }
+  const handleDeleteInProgress = (index: number) => {
+    let newProgress = [...inProgress]
+    newProgress.splice(index, 1)
+    setProgress(newProgress)
+  }
+  const handleDeleteDone = (index: number) => {
+    let newDone = [...done]
+    newDone.splice(index, 1)
+    setDone(newDone)
+  }
+
+  //function to add the inprogress todo to the inprogress state
+  const handleToProgress = (todo: string) => {
+    let newProgress = [...inProgress]
+    if (todo in inProgress === false) newProgress.push(todo)
+    setProgress(newProgress)
+
+    let newTodos = [...todos]
+    let index = newTodos.indexOf(todo)
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+
+  const handleDone = (task: string) => {
+    let newDone = [...done]
+    newDone.push(task)
+    setDone(newDone)
+
+    let newProgress = [...inProgress]
+    let index = newProgress.indexOf(task)
+    newProgress.splice(index, 1)
+    setProgress(newProgress)
   }
 
   return (
@@ -35,26 +71,45 @@ function Kanban() {
                 <div className="w-1/3">
                   <div className="bg-white shadow-md rounded-md p-3 h-full overflow-y-scroll">
                     <h2 className="text-center mb-5 text-amber-500 text-lg ">To Do</h2>
-                    {tasks.map((task, index) => (
-                      <div key={index} className="flex flex-row justify-between p-3 mb-2 border border-gray-300 rounded-md hover:shadow-red-200 shadow-xl ">
-                        <div style={{ color: 'black' }}>{task}</div>
-                        <div>
-                          <button className="bg-red-500 text-white rounded-md p-2" onClick={() => handleDeleteTask(index)}>Delete</button>
+                    {todos.map((todo, index) => (
+                      <div key={index} className="flex flex-row justify-between p-3 mb-2 border border-gray-300 rounded-md hover:shadow-violet-200 shadow-xl text-center ">
+                        <div style={{ color: 'black' }}>{todo}</div>
+                        <div className='text-sm text-center'>
+                          <button className=" rounded-md p-2" onClick={() => handleDone(todo)}>✅</button>
+                          <button className=" rounded-md p-2" onClick={() => handleToProgress(todo)}>🟠</button>
+                          <button className=" rounded-md p-2" onClick={() => handleDeletetodo(index)}>❌</button>
+
                         </div>
                       </div>
                     ))}
                   </div>
                 </div>
                 <div className="w-1/3" style={{ height: '65vh', marginLeft: '10px' }}>
-                  <div className="bg-white shadow-md rounded-md p-3 h-full">
+                  <div className="bg-white shadow-md rounded-md p-3 h-full overflow-y-scroll">
                     <h2 className="text-center text-orange-500 mb-5 text-lg">In Progress</h2>
-                    <div></div>
+                    {inProgress.map((task, index) => (
+                      <div key={index} className="flex flex-row justify-between p-3 mb-2 border border-gray-300 rounded-md hover:shadow-orange-200 shadow-xl text-center ">
+                        <div style={{ color: 'black' }}>{task}</div>
+                        <div className='text-sm text-center'>
+                          <button className=" rounded-md p-2" onClick={() => handleDone(task)}>✅</button>
+                          <button className=" rounded-md p-2" onClick={() => handleDeleteInProgress(index)}>❌</button>
+
+                        </div>
+                      </div>
+                    ))}
                   </div>
                 </div>
                 <div className="w-1/3" style={{ height: '65vh', marginLeft: '10px' }}>
-                  <div className="bg-white shadow-md rounded-md p-3 h-full">
+                  <div className="bg-white shadow-md rounded-md p-3 h-full overflow-y-scroll">
                     <h2 className="text-center text-green-500 mb-5 text-lg">Done</h2>
-                    <div></div>
+                    {done.map((task, index) => (
+                      <div key={index} className="flex flex-row justify-between p-3 mb-2 border border-gray-300 rounded-md hover:shadow-green-200 shadow-xl text-center ">
+                        <div style={{ color: 'black' }}>{task}</div>
+                        <div className='text-sm text-center'>
+                          <button className=" rounded-md p-2" onClick={() => handleDeleteDone(index)}>❌</button>
+                        </div>
+                      </div>
+                    ))}
                   </div>
                 </div>
               </div>
@@ -65,11 +120,11 @@ function Kanban() {
           <div className="">
             <input
               className='bg-white shadow-md rounded-lg p-3 w-11/12'
-              placeholder='Add Task'
+              placeholder='Add todo'
               type="text"
               value={input}
               onChange={e => setInput(e.target.value)}
-              onKeyPress={handleAddTask}
+              onKeyPress={handleAddtodo}
             />
           </div>
         </div>
